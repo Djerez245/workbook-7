64f8c11b10a33b8294ddf9762599781f8aafc876
Damian <jerezdamian640@gmail.com>
2024-11-21 17:47:55 -0500

    cleaned up code

--- carDealershipDatabaseTest.sql
+++ carDealershipDatabaseTest.sql
@@ -16,4 +16,4 @@ SELECT dealership_id FROM vehicles WHERE type = 'sedan';
 -- 6
 SELECT * FROM sales_contract sc
 JOIN vehicles v ON sc.vin = v.vin
-where v.dealership_id = 1 AND sc.vehicle_date BETWEEN date('2023-11-01') AND date('2024-11-01');
+WHERE v.dealership_id = 1 AND sc.vehicle_date BETWEEN date('2023-11-01') AND date('2024-11-01');
