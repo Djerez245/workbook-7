c09252dacdf1121abf7a90a1e79eee6fcf57f0ae
Damian <jerezdamian640@gmail.com>
2024-11-26 15:03:52 -0500

    finished nested queries excercise

--- nested-queries.sql
+++ nested-queries.sql
@@ -8,7 +8,27 @@ WHERE UnitPrice = (SELECT MAX(UnitPrice) FROM products);
 -- 2
 SELECT OrderID, ShipName, ShipAddress
 FROM orders
-WHERE ShipVia = (SELECT ShipperID FROM shippers WHERE CompanyName = 'Federal Shipping');
+WHERE ShipVia = (
+	SELECT ShipperID 
+	FROM shippers 
+    WHERE CompanyName = 'Federal Shipping');
 
 -- 3
-SELECT 
+SELECT orderID
+FROM `order details`
+WHERE productID = (
+    SELECT productID
+    FROM products
+    WHERE productName = 'Sasquatch Ale'
+);
+
+-- 4
+select LastName, FirstName
+from employees
+where employeeID = (select employeeID from orders where orderID = 10266);
+
+-- 5
+select contactName
+from customers 
+where customerID = (select customerID from orders where OrderID = 10266);
+    
